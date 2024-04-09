6df68a075bcc8ad513f2b387ee091c5547b768a8
lock2010 <64908381+lock2010@users.noreply.github.com>
2024-04-09 23:58:00 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
 
-    private static String msg = "Hello QA!!!!!!!";
+    private static String sms = "Hello QA!!!!!!!";
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(sms);
     }
 }
