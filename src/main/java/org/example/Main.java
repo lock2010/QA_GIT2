8ce1ba026228b9becaa34dc50cd4b4e6c6c18a10
Alexey Klester <lexlk2@bk.ru>
2024-04-09 23:54:31 +0700

    added method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA!");
+        System.out.println(getMessage());
+    }
+    private static String getMessage() {
+        return "Hello world, QA!";
     }
 }
