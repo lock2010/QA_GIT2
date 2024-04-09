521def892226e23cf81b598fab6761f2081eef77
lock2010 <64908381+lock2010@users.noreply.github.com>
2024-04-09 23:53:19 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
+
+    private static String msg = "Hello QA!!!!!!!"
     public static void main(String[] args) {
-        System.out.println("Hello QA!!!!!!!");
+        System.out.println(msg);
     }
 }
